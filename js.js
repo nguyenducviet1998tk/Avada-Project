04255ac729948a481e640d63f122078c43714e49
nguyenducviet1998tk <nguyenducviet1998tk@gmail.com>
2017-05-01 15:43:48 +0700

    About
    Fixx

--- js.js
+++ js.js
@@ -1,8 +1,15 @@
-/*JavaScript.JQuery code by Hoang Kim Duc*/
+/*JavaScript.JQuery code by Hoang Kim Duc & NGUYEN DUC VIET*/
 $(document).ready(function(){
+    //Tooltip
+    $('[data-toggle="tooltip"]').tooltip();
+    //   
+    if( $("#headingOne h4 a").attr("aria-expanded")=="true"){
+             $("#headingOne h4").removeClass("panel-title");
+            $("#headingOne h4").addClass("panel-title-minus");
+        }
     $(".panel-heading-collapse h4 a").click(function(e){
-        var objectclick=e.currentTarget.parentElement.parentElement.getAttribute("id");
-        if( $("#"+objectclick+" h4").attr("class")=="panel-title"){
+         var objectclick=e.currentTarget.parentElement.parentElement.getAttribute("id");
+        if( $("#"+objectclick+" h4 a").attr("aria-expanded")=="false"){
             /*reset về + */
              $(".panel-heading-collapse h4").removeClass("panel-title-minus");
             $(".panel-heading-collapse h4").addClass("panel-title");
@@ -10,10 +17,10 @@ $(document).ready(function(){
             /**/
             $("#"+objectclick+" h4").removeClass("panel-title");
             $("#"+objectclick+" h4").addClass("panel-title-minus");
-        }else if( $("#"+objectclick+" h4").attr("class")=="panel-title-minus"){
+        }else if( $("#"+objectclick+" h4 a").attr("aria-expanded")=="true"){
             $("#"+objectclick+" h4").removeClass("panel-title-minus");
             $("#"+objectclick+" h4").addClass("panel-title");
         }
     })
-    
+
 })
